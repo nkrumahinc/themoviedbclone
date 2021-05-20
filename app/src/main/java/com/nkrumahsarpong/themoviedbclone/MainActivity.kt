2a91aa55b0f6f0d35aee9630350c4a8ac6100d36
nkrumahinc <nkrumahthis@gmail.com>
2021-05-20 21:09:49 +0000

    finish MainActivity.kt

--- app/src/main/java/com/nkrumahsarpong/themoviedbclone/MainActivity.kt
+++ app/src/main/java/com/nkrumahsarpong/themoviedbclone/MainActivity.kt
@@ -2,10 +2,42 @@ package com.nkrumahsarpong.themoviedbclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val progressBar:ProgressBar = findViewById(R.id.progressbar)
+        val recyclerView:RecyclerView = findViewById(R.id.recyclerview)
+
+        val request = ServiceBuilder.buildService(TmdbEndpints::class.java)
+        val call = request.getMovies(getString(R.string.api_key))
+
+        call.enqueue(object:Callback<PopularMovies>{
+            override fun onResponse(call: Call<PopularMovies>, response: Response<PopularMovies>){
+                if (response.isSuccessful){
+                    progressBar.visibility = View.GONE
+                    recyclerView.apply{
+                        setHasFixedSize(true)
+                        layoutManager = LinearLayoutManager(this@MainActivity)
+                        adapter = MoviesAdapter(response.body()!!.results)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<PopularMovies>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "${t.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
